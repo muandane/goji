9518db0f69f6979c87448dc38bff65e2a27fd3f2
moualhi zine el abidine <zmoualhi@outlook.com>
2023-12-14 19:35:34 +0100

    fix 🐛 (pkg): fixed commits issue

--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/fatih/color v1.16.0
 	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.18.1
-	github.com/stretchr/testify v1.8.4
 )
 
 require (
@@ -22,7 +21,6 @@ require (
 	github.com/charmbracelet/glamour v0.6.0 // indirect
 	github.com/charmbracelet/lipgloss v0.9.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
-	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dlclark/regexp2 v1.10.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
@@ -42,7 +40,6 @@ require (
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
-	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
--- pkg/utils/askQuestions.go
+++ pkg/utils/askQuestions.go
@@ -7,74 +7,55 @@ import (
 	"github.com/muandane/goji/pkg/config"
 )
 
-var commitType string
-var commitScope string
-var commitSubject string
-
-func AskQuestions(config *config.Config, commitType, commitScope, commitSubject string) (string, error) {
-	var finalCommitType, finalCommitScope, finalCommitSubject string
+func AskQuestions(config *config.Config) (string, error) {
+	var commitType string
+	var commitScope string
+	var commitSubject string
 	commitTypeOptions := make([]huh.Option[string], len(config.Types))
 
 	for i, ct := range config.Types {
 		commitTypeOptions[i] = huh.NewOption[string](fmt.Sprintf("%-10s %-5s %-10s", ct.Name, ct.Emoji, ct.Description), fmt.Sprintf("%s %s", ct.Name, ct.Emoji))
 	}
-	if commitType != "" {
-		finalCommitType = commitType
-	} else {
-		promptType := huh.NewSelect[string]().
-			Title("Select the type of change you are committing:").
-			Options(commitTypeOptions...).
-			Value(&commitType)
-		err := promptType.Run()
+
+	promptType := huh.NewSelect[string]().
+		Title("Select the type of change you are committing:").
+		Options(commitTypeOptions...).
+		Value(&commitType)
+
+	err := promptType.Run()
+	if err != nil {
+		return "", err
+	}
+	// Only ask for commitScope if not in SkipQuestions
+	if !isInSkipQuestions("Scopes", config.SkipQuestions) {
+		promptScope := huh.NewInput().
+			Title("What is the scope of this change? (class or file name): (press [enter] to skip)").
+			CharLimit(50).
+			Placeholder("Example: ci, api, parser").
+			Value(&commitScope)
+
+		err = promptScope.Run()
 		if err != nil {
 			return "", err
 		}
 	}
-	if commitScope != "" {
-		finalCommitScope = commitScope
-	} else {
-		if !isInSkipQuestions("Scopes", config.SkipQuestions) {
-			promptScope := huh.NewInput().
-				Title("What is the scope of this change? (class or file name): (press [enter] to skip)").
-				CharLimit(50).
-				Placeholder("Example: ci, api, parser").
-				Value(&commitScope)
 
-			err := promptScope.Run()
-			if err != nil {
-				return "", err
-			}
-		}
-		// Only ask for commitScope if not in SkipQuestions
-	}
-	if commitSubject != "" {
-		finalCommitSubject = commitSubject
-	} else {
-		promptSubject := huh.NewInput().
-			Title("Write a short and imperative summary of the code changes: (lower case and no period)").
-			CharLimit(100).
-			Placeholder("Short description of your commit").
-			Value(&commitSubject)
+	promptSubject := huh.NewInput().
+		Title("Write a short and imperative summary of the code changes: (lower case and no period)").
+		CharLimit(100).
+		Placeholder("Short description of your commit").
+		Value(&commitSubject)
 
-		err := promptSubject.Run()
-		if err != nil {
-			return "", err
-		}
+	err = promptSubject.Run()
+	if err != nil {
+		return "", err
 	}
 
 	var commitMessage string
 	if commitScope == "" {
-		if commitSubject != "" {
-			commitMessage = fmt.Sprintf("%s: %s", finalCommitType, finalCommitSubject)
-		} else {
-			commitMessage = fmt.Sprintf("%s: %s", commitType, commitSubject)
-		}
+		commitMessage = fmt.Sprintf("%s: %s", commitType, commitSubject)
 	} else {
-		if commitSubject != "" {
-			commitMessage = fmt.Sprintf("%s (%s): %s", finalCommitType, finalCommitScope, finalCommitSubject)
-		} else {
-			commitMessage = fmt.Sprintf("%s (%s): %s", commitType, commitScope, commitSubject)
-		}
+		commitMessage = fmt.Sprintf("%s (%s): %s", commitType, commitScope, commitSubject)
 	}
 	return commitMessage, nil
 }
